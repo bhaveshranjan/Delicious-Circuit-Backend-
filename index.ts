d8afaa3bb9b93a87e612a1a6f40e9003f7e3a7c5
Bhavesh Ranjan <bhaveshranjanswork@gmail.com>
2024-03-11 16:43:41 +0530

    Routes

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import { AdminRoute, VendorRoute } from "./routes";
 
 const app = express();
 
-app.use('/',(req,res)=>{
-    return res.json("Hello I am Backend")
-})
+app.use('/admin',AdminRoute);
+app.use('/vendor',VendorRoute);
 
 app.listen(8000,()=>{
     console.log("App is running at 8000")
---
+++ routes/VendorRoute.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+
+const router = express.Router();
+
+router.get('/',(req: Request, res: Response, next: NextFunction) =>{
+
+    res.json({message: "Hello from Vendor"})
+})
+
+
+export {router as VendorRoute};
